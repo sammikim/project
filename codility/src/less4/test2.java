4c57e77050eedfcb5e82861a0e6bb8530032a693
Admin <Admin@samkim>
2018-08-20 16:55:56 +0900

    테스트 commit

--- codility/src/less4/test2.java
+++ codility/src/less4/test2.java
@@ -1,10 +1,7 @@
 package less4;
 
 public class test2 {
-	int[][] A = { [[1,2],[2,5],[2,6],[1,3],[1,4],[3,7]]};
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
